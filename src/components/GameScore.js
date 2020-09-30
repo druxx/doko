b308e40a943b752fc12601714769922cbe518221
behrens <ulf.behrens@cern.ch>
2020-10-01 00:58:30 +0200

    snapshot

--- src/components/GameScore.js
+++ src/components/GameScore.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Container, Grid, Paper, Typography } from '@material-ui/core';
+import { Container, Grid, Paper, Typography, Checkbox, TextField } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import CheckboxTable from './CheckboxTable';
 
 
 const styles = theme => ({
@@ -21,6 +22,12 @@ const styles = theme => ({
     container: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4),
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        marginBottom: theme.spacing(2),
+        width: '5ch'
     }
 })
 
@@ -32,9 +39,65 @@ const mapStateToProps = state => {
 
 class GameScore extends Component {
 
+    constructor(props) {
+        super(props);
+        this.pointsList = ['keine 120', 'keine 90', 'keine 60', 'keine 30', 'schwarz'];
+        this.announcedList = ['re', 'kontra', 'keine 120', 'keine 90', 'keine 60', 'keine 30', 'schwarz'];
+        const state = {
+            contra: false,
+            points: Array(this.pointsList.length).fill(false),
+            announced: Array(this.announcedList.length).fill(false),
+            result: 1
+        };
+        state.points[0] = true;
+        this.state = state;
+    }
+
+    componentDidMount() {
+        this.setState( {selectedPlayers: Array(this.props.players.length).fill(false)});
+    }
+
+    onPlayersClick = (event, index) => {
+        let newSelected = [...this.state.selectedPlayers];
+        newSelected[index] = !this.state.selectedPlayers[index];
+        this.setState({selectedPlayers: newSelected});
+    };
+
+    onPointsClick = (event, index) => {
+        let firstFalse = this.state.points.findIndex(element => element == false);
+        if (firstFalse == -1)
+            firstFalse = this.state.points.length;
+        const oldMax = firstFalse - 1;
+        let newSelected = [...this.state.points];
+        const selected = !this.state.points[index];
+        let newMax = 0;
+        if ( selected ) {
+            newMax = index;
+            for (let i = 0; i <= index; i++)
+                newSelected[i] = true;
+        }
+        else {
+            newMax = index - 1;
+            for (let i = index; i < newSelected.length; i++)
+                newSelected[i] = false;
+        }
+        this.setState({points: newSelected});
+        this.setState({result: this.state.result + newMax - oldMax});
+    };
+
+    onContraClicked = (event) => {
+        const contra = !this.state.contra;
+        this.setState({contra: contra});
+        if (contra)
+            this.setState({result: this.state.result + 1});
+        else
+            this.setState({result: this.state.result - 1});
+    }
+
     render() {
         const { classes, players } = this.props;
-        this.players = players;
+        const playerNames = players.map(player => player.name);
+        
 
         return (
             <div className={classes.content}>
@@ -42,9 +105,37 @@ class GameScore extends Component {
                 <Grid container spacing={10} direction="column" justify="space-evenly" alignItems="center">
                     <Grid container item xs={12} spacing={1} justify="space-evenly" alignItems='stretch'>
                         <Grid item xs={2}>
+                            <CheckboxTable title={'gewonnen:'} items={playerNames} selected={this.state.selectedPlayers} onClick={this.onPlayersClick}/>
+                        </Grid>
+                        <Grid item xs={2}>
                             <Paper>
-                                <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                                    Gewinner:
+                                <Typography component="h2" variant="h5" color="primary" align='center'>
+                                    gegen die Alten?
+                                </Typography>
+                                <Typography align='center'>
+                                    <Checkbox checked={this.state.contra} onClick={this.onContraClicked}/>
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <CheckboxTable title={'Punkte'} items={this.pointsList} selected={this.state.points} onClick={this.onPointsClick}/>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <CheckboxTable title={'Ansage'} items={this.announcedList} hideTable={true}/>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <Paper>
+                                <Typography component="h2" variant="h5" color="primary" align='center'>
+                                    Punkte
+                                </Typography>
+                                <Typography align='center'>
+                                <TextField
+                                    className={classes.textField}
+                                    inputProps={{style: {fontSize: '250%'}}}
+                                    label=''
+                                    value={this.state.result}
+                                    inputRef={(c) => {this.resultInput = c}}
+                                />
                                 </Typography>
                             </Paper>
                         </Grid>
